8529893225dbd10b9efb4cf8b4a0b36b2916b169
Hlib Omelchenko <omelchenkohlib@fastmail.com>
2026-09-12 21:37:29 +0200

    Mass Effect 2: Drop the gamut-clip credit the sibling ports do not carry

--- Source/Games/Mass Effect 2/main.cpp
+++ Source/Games/Mass Effect 2/main.cpp
@@ -1225,7 +1225,6 @@ public:
                   "\nRenoDX (HDR tonemap method)"
                   "\nDICE (HDR tonemapper)"
                   "\nMacLeod-Boynton hue emulation (RenoDX)"
-                  "\nLilium (gamut clip)"
                   "\nSMAA (Iryoku)"
                   "\nAMD FidelityFX (RCAS)"
                   "\ndgVoodoo2 (DirectX 9 -> 11 wrapper, required)");
