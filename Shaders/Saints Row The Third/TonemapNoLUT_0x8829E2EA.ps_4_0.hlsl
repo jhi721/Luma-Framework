1833240581bb753177b65937aebd389e424b3cf8
Hlib Omelchenko <omelchenkohlib@fastmail.com>
2026-09-23 21:49:24 +0200

    Saints Row IV Re-Elected: Remove the stale standalone Saints Row The Third copy

--- Shaders/Saints Row The Third/TonemapNoLUT_0x8829E2EA.ps_4_0.hlsl
+++
@@ -1,7 +0,0 @@
-// rl_hdr_09: final composite with bloom and the 1.49 shoulder but no colour LUT.
-#include "Luma_SR3_Tonemap.hlsl"
-
-void main(float4 pos : SV_Position, float2 uv : TEXCOORD0, out float4 o0 : SV_Target0)
-{
-   o0 = SR3_TonemapNoLUT(uv);
-}
--- Shaders/Saints Row The Third/Tonemap_0xDD93F990.ps_4_0.hlsl
+++
@@ -1,7 +0,0 @@
-// rl_hdr_13: final composite with bloom, the 1.49 shoulder and the colour LUT (PostProcess 1/2).
-#include "Luma_SR3_Tonemap.hlsl"
-
-void main(float4 pos : SV_Position, float2 uv : TEXCOORD0, out float4 o0 : SV_Target0)
-{
-   o0 = SR3_TonemapLUT(uv);
-}
